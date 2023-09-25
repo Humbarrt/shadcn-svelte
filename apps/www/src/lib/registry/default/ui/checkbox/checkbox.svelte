9f6ebd0f8ea4598d5069ff471426fd169c902f45
Maximilian Humpert <humpert764@gmail.com>
2023-09-25 23:08:36 +0200

    fix checkbox icon not being centered

--- apps/www/src/lib/registry/default/ui/checkbox/checkbox.svelte
+++ apps/www/src/lib/registry/default/ui/checkbox/checkbox.svelte
@@ -13,7 +13,7 @@
 
 <CheckboxPrimitive.Root
 	class={cn(
-		"peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50",
+		"peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50 flex justify-center items-center",
 		className
 	)}
 	bind:checked
